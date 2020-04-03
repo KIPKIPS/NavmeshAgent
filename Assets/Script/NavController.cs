8da3f6ab2ec3d55a172dd86f625749ad34269765
RECNEPS-KIPS <2432719656@qq.com>
2020-04-03 16:30:49 +0800

    实现了AI寻路的往返效果

--- Assets/Script/NavController.cs
+++ Assets/Script/NavController.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NavController : MonoBehaviour
-{
+public class NavController : MonoBehaviour {
+    public Transform destination;
+    public Vector3 targetPos;
+    public NavMeshAgent nav;
+
+    public Vector3 oriPos;
+    public bool tar = false;
+    void Awake() {
+        oriPos = transform.position;
+    }
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        targetPos = destination.position;
+        nav = GetComponent<NavMeshAgent>();
+        nav.SetDestination(targetPos);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (tar) {
+            nav.SetDestination(oriPos);
+        }
+        else {
+            nav.SetDestination(targetPos);
+        }
+        if (Vector3.Distance(transform.position,targetPos)<=0.1f) {
+            tar = true;
+        }
+        if (Vector3.Distance(transform.position, oriPos) <= 0.1f) {
+            tar = false;
+        }
     }
 }
