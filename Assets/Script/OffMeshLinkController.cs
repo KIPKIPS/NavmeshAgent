01f43ee3447ce1a21b49a4a8307073fc2ea46b98
RECNEPS-KIPS <2432719656@qq.com>
2020-04-04 12:50:58 +0800

    实现断层的传送效果
    使用OffMeshLink组件

---
+++ Assets/Script/OffMeshLinkController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OffMeshLinkController : MonoBehaviour {
+    public Transform destination;
+    public Vector3 targetPos;
+    public NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Start() {
+        targetPos = destination.position;
+        nav = GetComponent<NavMeshAgent>();
+        nav.SetDestination(targetPos);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
