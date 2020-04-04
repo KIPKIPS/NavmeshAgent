8d481d2eb7ecae06eae7d7992ade15b76d2ff3ce
RECNEPS-KIPS <2432719656@qq.com>
2020-04-04 16:29:57 +0800

    实现了寻路的动态开关

--- Assets/Script/OffMeshLinkController.cs
+++ Assets/Script/OffMeshLinkController.cs
@@ -7,15 +7,29 @@ public class OffMeshLinkController : MonoBehaviour {
     public Transform destination;
     public Vector3 targetPos;
     public NavMeshAgent nav;
+
+    private IEnumerator ie;
     // Start is called before the first frame update
     void Start() {
+        ie = OpenDoor();
         targetPos = destination.position;
         nav = GetComponent<NavMeshAgent>();
         nav.SetDestination(targetPos);
+        StartCoroutine(ie);
     }
 
     // Update is called once per frame
     void Update() {
 
+    }
+
+    IEnumerator OpenDoor() {
+        yield return new WaitForSeconds(8);
+        Debug.Log("open");
+        //若AI在开门之前就抵达门,则它的寻路目标需要重新设置
+        nav.areaMask = nav.areaMask|1<<4;//将寻路的第四层(Door)添加到可以寻路的层里面
+        //重新制定寻路的目标
+        nav.SetDestination(targetPos);
+        StopCoroutine(ie);
     }
 }
