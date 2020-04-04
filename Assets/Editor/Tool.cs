0e00282e03b7e859dba9fd2c7f59a82d3febda56
RECNEPS-KIPS <2432719656@qq.com>
2020-04-04 20:18:50 +0800

    使用阻挡来实现动态寻路

---
+++ Assets/Editor/Tool.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Tool:MonoBehaviour {
+    [MenuItem("Tool/Export Navmesh Data")]
+    public static void ExportNavMeshData() {
+        NavMeshTriangulation data = NavMesh.CalculateTriangulation();
+    }
+}
---
+++ Assets/Script/DynamicNavMeshObstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DynamicNavMeshObstacle : MonoBehaviour {
+    public Transform destination;
+    public Vector3 targetPos;
+    public NavMeshAgent nav;
+    public GameObject door;
+    private IEnumerator ie;
+    // Start is called before the first frame update
+    void Start() {
+        ie = OpenDoor();
+        targetPos = destination.position;
+        nav = GetComponent<NavMeshAgent>();
+        nav.SetDestination(targetPos);
+        StartCoroutine(ie);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator OpenDoor() {
+        yield return new WaitForSeconds(6);
+        door.transform.RotateAround(new Vector3(-5.178f, 4.47f, 3.75f), Vector3.up, 90);
+        Debug.Log("open");
+        StopCoroutine(ie);
+    }
+}
